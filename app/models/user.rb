255327e1033704b34817f9dc00cc3c49dcd89e90
Abhi Yerra <abhi@berkeley.edu>
2011-04-21 00:23:59 -0700

    Updated activities.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+  def add_activity(to, message)
+    activity = Activity.create(:to => to, :message => message)
+
+
+  end
 end
--- db/migrate/20110207011228_create_activities.rb
+++ db/migrate/20110207011228_create_activities.rb
@@ -1,6 +1,10 @@
 class CreateActivities < ActiveRecord::Migration
   def self.up
     create_table :activities do |t|
+      t.integer :from_id
+      t.integer :to_id
+
+      t.string :message
 
       t.timestamps
     end
